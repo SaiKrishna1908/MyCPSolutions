79c5aecf161553bc9de48e1ddc7ff8b95a0ecfc4
krishna <saikrishna1908@gmail.com>
2022-03-28 07:04:59 +0530

    LCS working

--- cp_solutions/subsequence.cpp
+++ cp_solutions/subsequence.cpp
@@ -12,94 +12,81 @@ using namespace std::chrono;
 1. How many states will be there in my dp ?
 ** There will be 2 states
 
-2. What does each state in my dp represent ? 
+2. What does each state in my dp represent ?
 ** dp[i][j] represents the maximum subsequence possible with string s of length s[0..i] and
     string t of length t[0..j].
 
 3. How do we transition to new state, i.e how do we compute dp[i]  using previous values ?
 ** The transition rules are as follows:
 
-  1. if s[i] == t[j] 
+  1. if s[i] == t[j]
 
-        the following statements says that dp[i][j] 
+        the following statements says that dp[i][j]
         is equal to 1 + maximum subsequence in s[i-1] t[j-1]
 
         dp[i][j] = dp[i-1][j-1] + 1
-      else 
+      else
         If they are not equal then max of dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-        dp[i-1][j] --> exclude i character from string s 
+        dp[i-1][j] --> exclude i character from string s
         dp[i][j-1] --> exclude j character from string t
 */
 
-void display(vector<vector<int>> arr) {
+int dp[3001][3001];  
 
-    for(int i=0;i<arr.size();i++) {
-        for(int j=0;j<arr[i].size();j++) {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-string ans;
-
-void print_string(vector<vector<int>> arr, string s, string t, int i, int j) {
-
-  if(i<=0 || j<=0) {
-    return;
-  }
-
-  if (s[i-1] == t[j-1]) {    
-    ans.push_back(s[i-1]);  
-    print_string(arr, s, t, i-1, j-1);
-  } else {
-    arr[i-1][j] > arr[i][j-1] ? print_string(arr,s,t,i-1,j) : print_string(arr,s,t,i,j-1);
-  }
-}
 
 void run() {
   string s,t;
   cin>>s>>t;
 
-  vector<vector<int>> dp(s.length()+1, vector<int>(t.length()+1,0));
-
-  // fill(dp.begin(), dp.end(), 0);
-
-// Why 
-  dp[0][0] = 0;
-  dp[0][1] = 0;
-  dp[1][0] = 0;
   for(int i=1;i<=s.length();i++) {
       for(int j=1;j<=t.length();j++) {
+        
+        if (!i || !j) {
+          dp[i][j] = 0;
+        }
 
         // if i
-        if(s[i-1] == t[j-1]) {
+        else if(s[i-1] == t[j-1]) {
             dp[i][j] = 1+ dp[i-1][j-1];
         }
         else {
           dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
-        
+
       }
   }
 
-  // print_string(dp,s,t,s.length(), t.length());
-  // reverse(ans.begin(), ans.end());
-  // cout<<ans<<endl;
+  int i=s.length(),j=t.length();
   
+  string ans = "";
+
+  while (i >0 && j>0) {    
+    if (s[i-1] == t[j-1]) {      
+      ans+=s[i-1];
+      i--;
+      j--;      
+  } else {
+    dp[i-1][j] > dp[i][j-1] ? (i--) : (j--);
+    }
+  }
+  // cout<<dp[s.length()][t.length()]<<endl;  
+  reverse(ans.begin(), ans.end());
+  cout<<ans<<endl;
+
 }
 
 int main() {
 
+  cin.tie(0)->sync_with_stdio(0);
   // cin.tie(0);
   // cin.sync_with_stdio(false);
-    
+
 
   auto start = high_resolution_clock::now();
-  
+
   run();
-  
+
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<microseconds>(stop - start);
 
