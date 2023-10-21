87467aad0c9c2d38556ed825ca114e5abb1e9d46
saikrishna1908 <saikrishna1908@gmail.com>
2023-10-21 12:05:53 -0500

    codeforces

---
+++ codeforces/133A.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define INF 1e9+7
+
+template < class c > struct rge { c b, e; };
+template < class c > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+struct debug {
+    
+    ~debug() { cerr << endl; }
+    template < class c > typename enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) { cerr << boolalpha << i; return * this; }
+    template < class c > typename enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) { return * this << range(begin(i), end(i)); }
+    template < class c, class b > debug & operator <<(pair < b, c > d) {
+        return * this << "(" << d.first << ", " << d.second << ")";
+    }
+    template < class c > debug & operator <<(rge<c> d) {
+        *this << "[";
+        for (auto it = d.b; it != d.e; ++it)
+            *this << ", " + 2 * (it == d.b) << *it;
+        return * this << "]";
+    }
+    
+};
+
+void print_vec(vector<int> arr) {
+  for(auto itr = arr.begin(); itr!=arr.end();itr++) {
+    cout<<(*itr)<<" ";
+  }
+  cout<<endl;
+}
+
+
+void run() {
+  string s;
+
+  cin>>s;
+
+  bool is_valid = false;
+
+  for(auto c : s) {
+    if (c == 'H' || c == 'Q' || c == '9') {
+      is_valid = true;
+      break;
+    }
+  }
+
+  if(is_valid) {
+    cout<<"YES"<<endl;
+  } else {
+    cout<<"NO"<<endl;
+  }
+}
+
+int main() {
+
+  // cin.tie(0);
+  // cin.sync_with_stdio(false);
+    
+
+  auto start = high_resolution_clock::now();
+  
+  run();
+  
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  // cout<<"time: "<<duration.count()/1e6<<endl;
+}
